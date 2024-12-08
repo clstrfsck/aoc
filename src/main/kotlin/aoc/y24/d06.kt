adfe7803877b18ab48828a51c662807ff1f1d259
Martin Sandiford <msandiford@gmail.com>
2024-12-08 07:06:15 +0000

    Adding day 8

--- src/main/kotlin/aoc/y24/d06.kt
+++ src/main/kotlin/aoc/y24/d06.kt
@@ -6,11 +6,11 @@ private data class Point(val y: Int, val x: Int)
 private data class Direction(val dy: Int, val dx: Int)
 private operator fun Point.plus(d: Direction) = Point(y + d.dy, x + d.dx)
 private fun Direction.right() = Direction(dx, -dy)
-private operator fun List<CharArray>.contains(p: Point): Boolean = p.y in indices && p.x in get(p.y).indices
-private operator fun List<CharArray>.get(p: Point) = if (contains(p)) this[p.y][p.x] else null
+private operator fun List<String>.contains(p: Point): Boolean = p.y in indices && p.x in get(p.y).indices
+private operator fun List<String>.get(p: Point) = if (contains(p)) this[p.y][p.x] else null
 private data class PathState(val p: Point, val d: Direction, val visited: MutableSet<Pair<Point, Direction>>)
 
-private fun List<CharArray>.getPath(start: Point) =
+private fun List<String>.getPath(start: Point) =
     generateSequence(PathState(start, Direction(-1, 0), mutableSetOf())) { (p, d, visited) ->
         if (contains(p)) {
             visited.add(p to d)
@@ -19,7 +19,7 @@ private fun List<CharArray>.getPath(start: Point) =
         } else null
     }.last().visited.map { it.first }.toSet()
 
-private tailrec fun List<CharArray>.isValid(o: Point, ps: PathState): Boolean {
+private tailrec fun List<String>.isValid(o: Point, ps: PathState): Boolean {
     if (!ps.visited.add(ps.p to ps.d)) return true
     if (!contains(ps.p)) return false
     val (newp, newd) = if (get(ps.p + ps.d) == '#' || ps.p + ps.d == o) ps.p to ps.d.right() else ps.p + ps.d to ps.d
@@ -27,7 +27,7 @@ private tailrec fun List<CharArray>.isValid(o: Point, ps: PathState): Boolean {
 }
 
 fun main() {
-    val grid = File("y24/y24d06.txt").readLines().map { it.toCharArray() }
+    val grid = File("y24/y24d06.txt").readLines()
     val start = grid.indices.flatMap { r ->
         grid[r].indices.mapNotNull { c -> if (grid[r][c] == '^') Point(r, c) else null }
     }.firstOrNull() ?: throw IllegalStateException("No start found")
---
+++ src/main/kotlin/aoc/y24/d08.kt
@@ -0,0 +1,25 @@
+package aoc.y24
+
+import java.io.File
+
+private data class P2(val y: Int, val x: Int)
+private data class V2(val dy: Int, val dx: Int)
+private operator fun P2.minus(v: V2) = P2(y - v.dy, x - v.dx)
+private operator fun P2.minus(p: P2) = V2(y - p.y, x - p.x)
+
+private fun List<P2>.pairs() = flatMapIndexed { i, a -> mapIndexedNotNull { j, b -> if (i != j) a to b else null } }
+private fun List<String>.has(p: P2): Boolean = p.y in indices && p.x in (this[p.y].indices)
+private fun List<String>.antinodes(p: P2, v: V2) = generateSequence(p) { it - v }.takeWhile { has(it) }
+
+fun main() {
+    val input = File("y24/y24d08.txt").readLines()
+
+    val elements = input.flatMapIndexed { y, r -> r.mapIndexed { x, c -> c to P2(y, x) } }
+    val map = elements.filter { it.first != '.' }.groupBy({ it.first }, { it.second })
+
+    val result1 = map.values.flatMap { it.pairs().map { (p1, p2) -> p1 - (p2 - p1) } }.filter { input.has(it) }
+    println("Result 1: ${result1.toSet().size}")
+
+    val result2 = map.values.flatMap { it.pairs().flatMap { (p1, p2) -> input.antinodes(p1, p2 - p1) } }
+    println("Result 2: ${result2.toSet().size}")
+}
