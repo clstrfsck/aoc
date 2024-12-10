cff75d4d5bb9058814caa3191cae6af647b5893a
Martin Sandiford <msandiford@gmail.com>
2024-12-10 08:46:43 +0000

    Improve performance of Day 7

--- src/main/kotlin/aoc/y24/d07.kt
+++ src/main/kotlin/aoc/y24/d07.kt
@@ -2,24 +2,30 @@ package aoc.y24
 
 import java.io.File
 
-private fun validEqn(target: Long, acc: Long, op: Char, args: List<Long>, ops: List<Char>): Boolean {
-    if (args.isEmpty()) return acc == target
+fun canInvertConcat(acc: Long, n: Long) = acc != n && acc.toString().endsWith(n.toString())
+fun invertConcat(acc: Long, n: Long) = acc.toString().dropLast(n.toString().length).toLong()
+
+private fun validEqn(acc: Long, op: Char, args: List<Long>, ops: List<Char>): Boolean {
+    val n = args.last()
+    if (args.size == 1) return acc == n
     val newAcc = when (op) {
-        '+' -> acc + args.first()
-        '*' -> acc * args.first()
-        '|' -> "$acc${args.first()}".toLong()
+        '+' -> if (acc >= n) acc - n else return false
+        '*' -> if (acc % n == 0L) acc / n else return false
+        '|' -> if (canInvertConcat(acc, n)) invertConcat(acc, n) else return false
         else -> throw IllegalArgumentException("Unknown operator: $op")
     }
-    return ops.any { validEqn(target, newAcc, it, args.drop(1), ops) }
+    return ops.any { validEqn(newAcc, it, args.dropLast(1), ops) }
 }
 
 fun main() {
     val input = File("y24/y24d07.txt").readLines()
     val equations = input.map { it.split(": ") }.map { (v, ns) -> v.toLong() to ns.split(" ").map { it.toLong() } }
 
-    val result1 = equations.filter { (v, ns) -> validEqn(v, 0, '+', ns, listOf('+', '*')) }.sumOf { it.first }
+    val ops1 = listOf('+', '*')
+    val result1 = equations.filter { (v, ns) -> ops1.any { validEqn(v, it, ns, ops1) } }.sumOf { it.first }
     println("Result 1: $result1")
 
-    val result2 = equations.filter { (v, ns) -> validEqn(v, 0, '+', ns, listOf('+', '*', '|')) }.sumOf { it.first }
+    val ops2 = listOf('+', '*', '|')
+    val result2 = equations.filter { (v, ns) -> ops2.any { validEqn(v, it, ns, ops2) } }.sumOf { it.first }
     println("Result 2: $result2")
 }
